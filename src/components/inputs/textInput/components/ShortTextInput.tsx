a80267f1bafa99522af3224da60abf711b1565e4
汪志鹏 <13476859295@163.com>
2023-08-11 21:56:10 +0800

    xx

--- src/components/inputs/textInput/components/ShortTextInput.tsx
+++ src/components/inputs/textInput/components/ShortTextInput.tsx
@@ -21,9 +21,50 @@
 //    )
 //}
 
+//import { splitProps } from 'solid-js'
+//import { JSX } from 'solid-js/jsx-runtime'
+//import { onMount, onCleanup } from 'solid-js'
+
+//type ShortTextInputProps = {
+//    ref: HTMLTextAreaElement | undefined
+//    onInput: (value: string) => void
+//} & Omit<JSX.TextareaHTMLAttributes<HTMLTextAreaElement>, 'onInput'>
+
+//export const ShortTextInput = (props: ShortTextInputProps) => {
+//    const [local, others] = splitProps(props, ['ref', 'onInput'])
+
+//    // 创建一个引用来引用textarea元素
+//    let textareaRef: HTMLTextAreaElement | undefined
+
+//    const adjustHeight = () => {
+//        if (textareaRef) {
+//            textareaRef.style.height = 'auto'
+//            textareaRef.style.height = `${textareaRef.scrollHeight}px`
+//        }
+//    }
+
+//    // 在挂载后和输入时调整高度
+//    onMount(adjustHeight)
+//    local.onInput = (value: string) => {
+//        props.onInput(value)
+//        adjustHeight()
+//    }
+
+//    return (
+//        <textarea
+//            rows = "1"
+//            ref={el => textareaRef = el}
+//            class='focus:outline-none bg-transparent px-4 py-4 flex-1 w-full text-input'
+//            style={{ 'font-size': '16px', 'overflow-y': 'hidden', 'resize': 'none' }}  // 设置overflow-y为hidden以隐藏滚动条
+//            onInput={(e) => local.onInput(e.currentTarget.value)}
+//            {...others}
+//        />
+//    )
+//}
+
 import { splitProps } from 'solid-js'
 import { JSX } from 'solid-js/jsx-runtime'
-import { onMount, onCleanup } from 'solid-js'
+import { onMount } from 'solid-js'
 
 type ShortTextInputProps = {
     ref: HTMLTextAreaElement | undefined
@@ -45,18 +86,20 @@ export const ShortTextInput = (props: ShortTextInputProps) => {
 
     // 在挂载后和输入时调整高度
     onMount(adjustHeight)
-    local.onInput = (value: string) => {
+
+    const handleInput = (e: Event) => {
+        const value = (e.currentTarget as HTMLTextAreaElement).value
         props.onInput(value)
         adjustHeight()
     }
 
     return (
         <textarea
-            rows = "1"
+            rows="1"
             ref={el => textareaRef = el}
             class='focus:outline-none bg-transparent px-4 py-4 flex-1 w-full text-input'
             style={{ 'font-size': '16px', 'overflow-y': 'hidden', 'resize': 'none' }}  // 设置overflow-y为hidden以隐藏滚动条
-            onInput={(e) => local.onInput(e.currentTarget.value)}
+            onInput={handleInput}
             {...others}
         />
     )
